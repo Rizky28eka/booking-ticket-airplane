8b96322a003e2161780fef6c54b5b24d51e9dc48
Rizky28eka <eka28rizky03haryadi04@gmail.com>
2023-09-24 17:57:25 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/booking_ticket_airplane_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.booking_ticket_airplane_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import google_fit
+import path_provider_foundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  GoogleFitPlugin.register(with: registry.registrar(forPlugin: "GoogleFitPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+}
